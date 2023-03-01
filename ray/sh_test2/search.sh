0891518198d50e4a8d2c97c7df160a743a1d1d23
穆凡浩 <21373490@buaa.edu.cn>
2023-03-01 19:53:21 +0800

    try2

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer.
-grep -n int $1|awk -F: '{print $1}' > $2
+grep -n $2 $1|awk -F: '{print $1}' > $3
--- src/sh_test/hello_os.sh
+++ src/sh_test/hello_os.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-touch $2
 sed -n '8p' $1 > $2
 sed -n '32p' $1 >> $2
 sed -n '128p' $1 >> $2
