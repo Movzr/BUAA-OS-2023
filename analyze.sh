6fb0a07a76a3a4bcd9a2ecc41df568e6265ac072
穆凡浩 <21373490@buaa.edu.cn>
2023-03-06 20:45:18 +0800

    yrt

--- analyze.sh
+++ analyze.sh
@@ -2,27 +2,26 @@
 
 if [ $# -eq 1 ];
 then
-    grep -E WARN.*|ERROR.* $1 > bug.txt
+    grep -E "WARN.*|ERROR.*" $1 > bug.txt
  
 else
     case $2 in
     "--latest")
         # Your code here. (2/4)
-    tail -n, --lines=5 $1     
+    tail -n 5 $1     
     ;;
     "--find")
         # Your code here. (3/4)
     grep $3 $1 > $3.txt 
     ;;
     "--diff")
-        # Your code here. (4/4)
-    	a=diff -q $1 $3
-	if [ a -eq 0 ]
-	then 
-		echo same
-	else
-		echo different
-	fi
+    	diff -s  $1 $3
+	#if [ $a -eq 0 ]
+	#then
+	#	echo same
+	#else
+	#	echo different
+	#fi
     ;;
     esac
 fi
