768dca06c3c904a8c7ad4402707da996b5c6edd8
穆凡浩 <21373490@buaa.edu.cn>
2023-03-06 20:21:37 +0800

    f

--- analyze.sh
+++ analyze.sh
@@ -2,21 +2,27 @@
 
 if [ $# -eq 1 ];
 then
-    # Your code here. (1/4)
-
+    grep -E WARN.*|ERROR.* $1 > bug.txt
+ 
 else
     case $2 in
     "--latest")
         # Your code here. (2/4)
-
+    tail -n, --lines=5 $1     
     ;;
     "--find")
         # Your code here. (3/4)
-
+    grep $3 $1 > $3.txt 
     ;;
     "--diff")
         # Your code here. (4/4)
-
+    	a=diff -q $1 $3
+	if [ a -eq 0 ]
+	then 
+		echo same
+	else
+		echo different
+	fi
     ;;
     esac
 fi
