3db2bea46b8d74c387b8b8250f6461f62393fb6a
穆凡浩 <21373490@buaa.edu.cn>
2023-04-16 21:26:50 +0800

    lab3_try4

--- kern/env.c
+++ kern/env.c
@@ -240,7 +240,7 @@ int env_alloc(struct Env **new, u_int parent_id) {
 	e=LIST_FIRST(&env_free_list);
 	/* Step 2: Call a 'env_setup_vm' to initialize the user address space for this new Env. */
 	/* Exercise 3.4: Your code here. (2/4) */
-	env_setup_vm(e);
+	try(env_setup_vm(e));
 	/* Step 3: Initialize these fields for the new Env with appropriate values:
 	 *   'env_user_tlb_mod_entry' (lab4), 'env_runs' (lab6), 'env_id' (lab3), 'env_asid' (lab3),
 	 *   'env_parent_id' (lab3)
