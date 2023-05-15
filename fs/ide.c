72ab70755c9ab738ca70745a0bd33a188ac4b07a
穆凡浩 <21373490@buaa.edu.cn>
2023-05-15 20:54:32 +0800

    try1

--- fs/ide.c
+++ fs/ide.c
@@ -11,50 +11,6 @@ int excel[31];
 int map[31];
 int flush[31];
 int allZero[512];
-void ssd_init(){
-	for(int i=0;i<32;i++){
-		excel[i]=-1;
-		map[i]=0;
-		flush[i]=0;
-	}
-	for(int i=0;i<512;i++){
-		allZero[i]=0;
-	}
-}
-int ssd_read(u_int logic_no, void *dst){
-	int result=excel[logic_no];
-	if(result<0){
-		return -1;
-	}
-	ide_read(0,result,dst,1);
-	return 0;
-}
-
-void ssd_write(u_int logic_no, void *src){
-	int r = excel[logic_no];
-        if(r<0) {
-                r= alloc_pblock();
-		excel[logic_no] = r;
-		map[r]=1;
-		ide_write(0,r,src,1);
-        }else{
-		ssd_erase(logic_no);
-		r= alloc_pblock();
-                excel[logic_no] = r;
-                map[r]=1;
-             	ide_write(0,r,src,1);
-	}
-}
-void ssd_erase(u_int logic_no) {
-	int r = excel[logic_no];
-	if(r<0) {
-		return ;
-	}
-	ide_write(0,r,(void *)allZero,1);
-	map[r]=0;
-	flush[r]+=1;
-	excel[logic_no]=-1;
-}
 
 int alloc_pblock(){
 	int min=99999999;
@@ -99,6 +55,96 @@ int alloc_pblock(){
 		return target;
 	}
 }
+
+void ssd_init(){
+	for(int i=0;i<32;i++){
+		excel[i]=-1;
+		map[i]=0;
+		flush[i]=0;
+	}
+	for(int i=0;i<512;i++){
+		allZero[i]=0;
+	}
+}
+int ssd_read(u_int logic_no, void *dst){
+	int result=excel[logic_no];
+	if(result<0){
+		return -1;
+	}
+	ide_read(0,result,dst,1);
+	return 0;
+}
+
+void ssd_write(u_int logic_no, void *src){
+	int r = excel[logic_no];
+        if(r<0) {
+                r= alloc_pblock();
+		excel[logic_no] = r;
+		map[r]=1;
+		ide_write(0,r,src,1);
+        }else{
+		ssd_erase(logic_no);
+		r= alloc_pblock();
+                excel[logic_no] = r;
+                map[r]=1;
+             	ide_write(0,r,src,1);
+	}
+}
+void ssd_erase(u_int logic_no) {
+	int r = excel[logic_no];
+	if(r<0) {
+		return ;
+	}
+	ide_write(0,r,(void *)allZero,1);
+	map[r]=0;
+	flush[r]+=1;
+	excel[logic_no]=-1;
+}
+/*
+int alloc_pblock(){
+	int min=99999999;
+	int target=0;
+	for(int i=0;i<32;i++){
+		if(map[i]==0){
+			if(flush[i]<min){
+				min=flush[i];
+				target=i;
+			}
+		}
+	}
+	if(min>=5){
+		char buf[1024];
+		int m=99999999;
+		int t=0;
+		for(int i=0;i<32;i++){
+			if(map[i]==1){
+				if(flush[i]<m){
+					m=flush[i];
+					t=i;
+				}
+			}
+		}
+		ide_read(0,t,(void *)buf,1);
+		ide_write(0,target,(void *)buf,1);
+		map[target]=1;
+		int logic;
+		for(int i=0;i<32;i++){
+			if(excel[i]==t){
+				logic=i;
+				break;
+			}
+		}
+		excel[logic]=target;
+		ide_write(0,t,(void *)allZero,1);
+		map[t]=0;
+		flush[t]+=1;
+		return t;
+	}
+	else{
+		return target;
+	}
+}
+*/
 // Overview:
 //  read data from IDE disk. First issue a read request through
 //  disk register and then copy data from disk buffer
